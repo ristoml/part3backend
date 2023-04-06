1326f6b838f3bbbd9afcae262b04ce893d170ea6
ristoml <ristoml@metropolia.fi>
2023-04-06 21:58:28 +0300

    fix port

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.send()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
